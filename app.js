1aa47c0e816a3ed1757afe6828ee896e94479c3e
DawoudTormos <103008170+DawoudTormos@users.noreply.github.com>
2025-03-22 19:19:56 +0200

    Adding a .env file and dotenv library.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const dotenv = require("dotenv");
 
 
 const app = express();
@@ -15,3 +16,6 @@ app.get("/", (req, res) => {
 
 const PORT = /*process.env.PORT ||*/ 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}\nhttp://localhost:${PORT}`));
+
+
+
