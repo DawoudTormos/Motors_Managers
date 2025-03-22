cc383413c2615ba74d8c034c61d57d0294c57f07
DawoudTormos <103008170+DawoudTormos@users.noreply.github.com>
2025-03-22 19:36:20 +0200

    Configuring PostgreSQL db connection.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const pool = require("./db");
 
 dotenv.config();
 
@@ -8,15 +9,27 @@ const app = express();
 app.use(express.json());
 
 
-app.get("/", (req, res) => {
+app.get("/test", async (req, res) => {
 
-    res.send("API is running...");
+    //res.send("API is running...");
+
+    try {
+        const result = await pool.query("SELECT * FROM admins");
+        res.json(result.rows);
+    } catch (err) {
+        res.send("Error!");
+        console.error(err.message);
+    }
 
 });
 
 
 const PORT = process.env.PORT ||5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}\nhttp://localhost:${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}\nhttp://localhost:${PORT}`)
+    console.log(`\nTest route: http://localhost:${PORT}/test`)
+}
+);
 
 
 
